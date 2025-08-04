d3b52642a9dc6228b1403b9fab315924b9a2d605
wyratung <leductung2015@gmail.com>
2025-08-04 16:01:40 +0700

    [EC-1] add global exception handler for project

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,4 @@
+using Common.Behaviors;
 using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,8 +6,9 @@ var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
-
+builder.Services.AddValidatorsFromAssembly(assembly);
 builder.Services.AddCarter();
 
 builder.Services.AddMarten(opts =>
@@ -16,5 +18,6 @@ builder.Services.AddMarten(opts =>
 var app = builder.Build();
 
 app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
